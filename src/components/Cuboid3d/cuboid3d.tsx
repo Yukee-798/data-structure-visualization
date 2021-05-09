15d43bfdc5454689b8fb119db7ab28a7a08a9aa6
Yukee-798 <909397116@qq.com>
2021-05-09 22:39:46 +0800

    Update cube

--- src/components/Cuboid3d/cuboid3d.tsx
+++ src/components/Cuboid3d/cuboid3d.tsx
@@ -1,85 +1,126 @@
 import * as THREE from 'three'
-import ReactDOM from 'react-dom'
 import React, { useEffect, useRef, useState } from 'react'
 import { animated, useSpring, config } from '@react-spring/three'
 import { RoundedBox, Text } from "@react-three/drei";
 import { useFrame } from '@react-three/fiber'
+import { IColorConfig } from '../../types';
+
+
+
+
+interface IArrCubeConfig {
+    value: string
+    sortIndex: number;
+    startPos: any;
+}
 
 
 interface ICuboid3d {
-    position?: any;
-    data?: string;
-    // ref?: any;
-    defColor?: string;
+    /* 颜色配置 */
+    colorConfig?: IColorConfig;
+    /* 数组cube配置 */
+    arrCubeConfig?: IArrCubeConfig;
 
+    /* 通用配置 */
+    position?: any;
+    isActive?: boolean;
+    isLock?: boolean;
 }
 
 const Cuboid3d: React.FC<ICuboid3d> = (props) => {
 
-    // 获取 mesh 实例
+    const {
+        position,
+        isActive,
+        isLock,
+        arrCubeConfig,
+        colorConfig,
+    } = props;
+
     const [isHover, setIsHover] = useState(false)
-    const [isActive, setIsActive] = useState(false)
+    const [isClick, setIsClick] = useState(false)
+    console.log(isLock);
+    /* 扫描数组的时候，如果改变了 active 属性，则给它设置一个点击效果 */
+    useEffect(() => {
+        isActive ? setIsClick(true) : setIsClick(false);
+    }, [isActive])
 
-    const { position, data, defColor } = props;
-
-    // 配置动画效果
+    /* 配置扩缩动画效果 */
     const { scale } = useSpring({
-        
         // scale 值改变的时候会平滑过渡
-        scale: isActive ? 1.5 : 1,
+        scale: isClick ? 1.15 : 1,
         // 添加颤抖效果
         config: config.wobbly
     });
 
+    /* 配置颜色过渡效果 */
     const { color } = useSpring({
-        color: isActive ? '#2c9678' : isHover ? 'skyblue' : 'wheat'
+        color: (
+            isClick ? colorConfig?.activeColor :
+            isHover ? colorConfig?.hoverColor : 
+            isLock ? colorConfig?.lockColor : colorConfig?.defaultColor
+        )
     })
-
-
-
 
     // 开启渲染循环，让 mesh 沿着 x 轴转动
     // useFrame(({clock}) => {
     //     meshRef.current.rotation.x = clock.getElapsedTime();
     // })
-    return (
-        <animated.mesh
-            scale={scale}
-            position={position}
-            // ref={ref}
-        >
-            <Text
-                fontSize={0.5}
-                color='black'
+
+    if (arrCubeConfig) {
+        return (
+            <animated.mesh
+                scale={scale}
+                position={position}
+                // trans
+                // ref={ref}
             >
-                {data}
-            </Text>
-            <RoundedBox
-                args={[2, 1, 1]}
-                onClick={() => {
-                    setIsActive(!isActive)
-                }}
-                onPointerOver={() => setIsHover(true)}
-                onPointerOut={() => setIsHover(false)}
+                <Text
+                    fontSize={0.5}
+                    color='black'
+                >
+                    {arrCubeConfig?.value}
+                </Text>
+                <RoundedBox
+                    args={[2, 1, 1]}
+                    onClick={() => {
+                        setIsClick(!isClick)
+                    }}
+                    onPointerOver={() => setIsHover(true)}
+                    onPointerOut={() => setIsHover(false)}
 
-            >
-                <animated.meshPhongMaterial
-                    color={defColor || color}
-                    //         vertexColors={true}
+                >
+                    <animated.meshPhongMaterial
+                        color={color}
+                        opacity={0.5}
+                        transparent={true}
+                        // vertexColors={true}
+                        // color={[0xffff00, 0xff00ff, 0x00ffff]}
+                        // specular={new THREE.Color(0x4488ee)}
+                        // shininess={12}
+                        // wireframeLinewidth={3}
+                        // wireframe={true}
+                    />
+                </RoundedBox>
+            </animated.mesh>
+        )
+    } else {
+        return (
+            <></>
+        )
+    }
+}
 
-                    //         color={[0xffff00, 0xff00ff, 0x00ffff]}
-                    //         // color='skyblue'
-                    //         specular={new THREE.Color(0x4488ee)}
-                    //         shininess={12}
-                    opacity={0.5}
 
-                    //         // wireframeLinewidth={3}
-                    //         // wireframe={true}
-                    transparent={true}
-                />
-            </RoundedBox>
-        </animated.mesh>
-    )
+Cuboid3d.defaultProps = {
+    colorConfig: {
+        defaultColor: 'wheat',
+        activeColor: 'orange',
+        hoverColor: 'skyblue',
+        lockColor: 'purple'
+    },
+    position: [0, 0, 0],
+    isLock: false,
 }
 
 export default Cuboid3d;
