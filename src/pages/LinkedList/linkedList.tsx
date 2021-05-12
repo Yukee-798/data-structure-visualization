d309823899917f64fd1fd823ac658a5e27820a88
Yukee-798 <909397116@qq.com>
2021-05-12 23:30:25 +0800

    Init linkedList

--- src/pages/LinkedList/linkedList.tsx
+++ src/pages/LinkedList/linkedList.tsx
@@ -1,9 +1,12 @@
+import Scene3d from '../../components/Scene3d/scene3d';
 import './linkedList.scss'
 
 const LinkedList = () => {
     return (
         <div className='linkedList-warp'>
-            linkedList
+            <Scene3d>
+            
+            </Scene3d>
         </div>
     )
 }
