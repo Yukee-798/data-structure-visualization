e6ee608b43bb9a56bde1a64412327057ec669caf
Yukee-798 <909397116@qq.com>
2021-05-13 17:16:57 +0800

    Update scene3d.

--- src/components/Scene3d/scene3d.tsx
+++ src/components/Scene3d/scene3d.tsx
@@ -2,7 +2,6 @@ import * as THREE from 'three'
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Suspense, useEffect, useRef, useState } from 'react';
 import { Environment, OrbitControls, PerspectiveCamera, Reflector, TransformControls } from "@react-three/drei";
-import { useSpring } from '@react-spring/three';
 import './scene3d.scss'
 
 const Scene3d: React.FC = (props) => {
