9b9b7e1b8a4d09576d090b9d86d96c671c905a0d
Yukee-798 <909397116@qq.com>
2021-05-14 23:39:08 +0800

    Refactor console.

--- src/components/Console/console.tsx
+++ src/components/Console/console.tsx
@@ -1,108 +1,37 @@
 import { Button, Drawer } from "antd";
-import { DataStrucTypes, HomeItemTypes, IBaseProps, OperaTypes } from "../../types";
+import { DataStrucTypes, IBaseProps } from "../../types";
 import './console.scss'
 
 interface IConsole extends IBaseProps {
-    /* 控制台类型 */
-    type: HomeItemTypes;
-    /* 点击控制台按钮时的回调 */
-    onOpera: (operaType: OperaTypes) => void;
-    /* 是否禁用控制台操作 */
+    /** 是否禁用控制台操作 */
     disabled?: boolean;
-    /* 禁用控制台操作时的节点渲染 */
-
+    /** 禁用控制台操作时的节点渲染 */
+    occupition?: React.ReactNode
 }
 
 const Console: React.FC<IConsole> = (props) => {
-    const { type, disabled, onOpera } = props;
-    switch (type) {
-        case 'sort':
-            return (
-                // <div className='console-warp' >
-                // <>
-                //     <Button
-                //         className='console-trigger'
-                //     >
-                //         操作台
-                //     </Button>
-                //     <Drawer
-                //         className='console'
-                //         title='操作台'
-                //         placement='right'
-                //         visible={false}
-                //         getContainer={false}
-                //     >
+    const { disabled, occupition, children } = props;
 
-                //     </Drawer>
-                // </>
-                // </div>
-                disabled ? <>操作进行中...</> : <>
-                    <Button onClick={() => { onOpera(OperaTypes.Random) }}>随机生成</Button>
-                    <Button>添加</Button>
-                    <Button>删除</Button>
-                    <Button onClick={() => { onOpera(OperaTypes.BubbleSort) }}>冒泡排序</Button>
-                    <Button onClick={() => { onOpera(OperaTypes.SelectSort) }}>选择排序</Button>
-                    <Button onClick={() => { onOpera(OperaTypes.Recover) }}>恢复</Button>
-                </>
-            )
-
-        case 'linkedList':
-            return (
-                <div></div>
-            )
-        case 'stack':
-            return (
-                <div></div>
-            )
-        case 'queue':
-            return (
-                <div></div>
-            )
-
-
-
-        case 'avlTree':
-            return (
-                <div></div>
-            )
-        case 'bPlusTree':
-            return (
-                <div></div>
-            )
-        case 'bTree':
-            return (
-                <div></div>
-            )
-        case 'binaryHeap':
-            return (
-                <div></div>
-            )
-        case 'binarySearch':
-            return (
-                <div></div>
-            )
-        case 'binarySearchTree':
-            return (
-                <div></div>
-            )
-        case 'graph':
-            return (
-                <div></div>
-            )
-        case 'graphTraverse':
-            return (
-                <div></div>
-            )
-        case 'hashTable':
-            return (
-                <div></div>
-            )
-        case 'redBlackTree':
-            return (
-                <div></div>
-            )
-
-    }
+    return (
+        <div className='console-warp'>
+            {disabled ?
+                <div className='mask'>
+                    {occupition}
+                </div> : <></>}
+            {children}
+        </div>
+    )
 }
+
+/* 
+            <Button onClick={() => { onOpera(OperaTypes.Random) }}>随机生成</Button>
+            <Button>添加</Button>
+            <Button>删除</Button>
+            <Button onClick={() => { onOpera(OperaTypes.BubbleSort) }}>冒泡排序</Button>
+            <Button onClick={() => { onOpera(OperaTypes.SelectSort) }}>选择排序</Button>
+            <Button onClick={() => { onOpera(OperaTypes.Recover) }}>恢复</Button>
+*/
+
+
 
 export default Console;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,11 @@
 import React from "react";
 
-/* 设置 cube 之间的间距 */
+/** 设置 cube 之间的间距 */
 export const CUBE_INTERVAL_DISTANCE = 2;
-/* 设置 3d 物品在 scene 中的基准 y 轴坐标 */
+/** 设置 3d 物品在 scene 中的基准 y 轴坐标 */
 export const BASE_POSY = -2;
-/* 设置遍历节点时的速度 */
-export const TRAVERSE_SPEED = 300;
+/** 遍历细节数组，分发任务的间隔时间 */
+export const DISPATCH_INTERVAL = 300;
 
 
 export interface IBaseProps {
@@ -13,10 +13,10 @@ export interface IBaseProps {
     className?: string;
 }
 
-/* 用于识别控制台类型 */
+/** 用于识别控制台类型 */
 export type HomeItemTypes = 'avlTree' | 'binaryHeap' | 'binarySearch' | 'binarySearchTree' | 'bPlusTree' | 'bTree' | 'graph' | 'graphTraverse' | 'hashTable' | 'linkedList' | 'queue' | 'redBlackTree' | 'sort' | 'stack';
 
-/* 控制台操作类型 */
+/** 控制台操作类型 */
 export enum OperaTypes {
     // 增删改查
     Add,
@@ -35,7 +35,7 @@ export enum OperaTypes {
 
 }
 
-/* 数据结构类型枚举同时也是tag的类型 */
+/** 数据结构类型枚举同时也是tag的类型 */
 export enum DataStrucTypes {
     Sort,
     Array,
@@ -67,7 +67,7 @@ export enum DataStrucTypes {
 
 
 
-/* 3d几何体通用配置 */
+/** 3d几何体通用配置 */
 export interface IGeometryProps {
     colorConfig?: IColorConfig
     position?: any;
@@ -77,13 +77,13 @@ export interface IGeometryProps {
 }
 
 export interface IColorConfig {
-    /* 默认颜色 */
+    /** 默认颜色 */
     defaultColor: string;
-    /* hover颜色 */
+    /** hover颜色 */
     hoverColor: string;
-    /* active颜色 */
+    /** active颜色 */
     activeColor: string;
-    /* lock颜色 */
+    /** lock颜色 */
     lockColor: string;
 }
 
