680e5ecefa6cb6ac79bcde979b1f6f5ed17d9e9d
Yukee-798 <909397116@qq.com>
2021-05-15 19:01:40 +0800

    Optimize some codes.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,42 +1,20 @@
 import { useEffect, useRef, useState } from "react"
 
-// 根据传入的时间获取多少时间前发布的
-export function getTime(time: string): string {
-    const minutes = 1000 * 60
-    const hours = minutes * 60
-    const days = hours * 24
-    const months = days * 30
-    const years = days * 365
-    let res: string = '刚刚'
-    let createTime = Date.parse(time)
-    let nowTime = Date.parse((new Date()).toString())
-    let subTime = nowTime - createTime
-    let y = subTime / years
-    let d = subTime / days
-    let mon = subTime / months
-    let h = subTime / hours
-    let m = subTime / minutes
-    const dateArr = [
-        { time: y, des: '年之前' },
-        { time: mon, des: '个月之前' },
-        { time: d, des: '天之前' },
-        { time: h, des: '小时之前' },
-        { time: m, des: '分钟之前' }
-    ]
-    for (let i = 0; i < dateArr.length; i++) {
-        if (dateArr[i].time > 1) {
-            res = Math.round(dateArr[i].time) + dateArr[i].des
-            break
-        }
-    }
-    return res
-}
-
-/* 随机生成一个 start ~ end 的整数 */
+/** 随机生成一个 start ~ end 的整数 */
 export function randomNum(start: number, end: number): number {
     return +(Math.random() * (end - start) + start).toFixed(0);
 }
 
+/** 随机生成长度为 3 ～ 10 的数组 */
+export function randomArr(): number[] {
+    const arr = [];
+    for (let i = 0; i < randomNum(3, 10); i++) {
+        arr.push(randomNum(3, 40));
+    }
+    return arr;
+}
+
+/** 判断某个元素是否 hover */
 export function useHover() {
     const [value, setValue] = useState(false);
     const ref = useRef<any>();
@@ -58,58 +36,3 @@ export function useHover() {
     );
     return [ref, value];
 }
-
-/*
-    const data = [
-        [
-            {
-                src:
-                tag:
-                title:
-                type:
-                onClick
-            },
-            {},
-            {},
-            {}
-        ],
-        [
-            {},
-            {},
-            {},
-            {}
-        ],
-        [
-            {},
-            {},
-            {},
-            {}
-        ],
-        [
-            {},
-            {},
-            {},
-            {}
-        ]
-    ]
-*/
-
-/*
-0000   0
-1111   1
-2222   2
-3      3
-
-*/
-
-// export function getHomeItemData(): IHomeItemProps[][] {
-//     let length = DataStrucTypes.Stack as number + 1;
-    
-//     const data: IHomeItemProps[][] = [];
-//     for (let i = 0; i < Math.ceil(data.length / 4); i++) {
-//         data.push(new Array<IHomeItemProps>(4).map((item, i) => ({
-
-//         })))
-//     }
-    
-// }
--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,20 +1,11 @@
 import { randomNum } from ".";
-import { ActionTypes } from "../pages/Sort/sort";
-import { CUBE_INTERVAL_DISTANCE, ICube } from "../types";
+import {ISortCube } from "../pages/Sort/sort";
+import { ActionTypes, SORT_CUBE_INTERVAL_DISTANCE } from "../types";
 
-/* 随机生成长度为 3 ～ 10 的数组 */
-export function randomArr(): number[] {
-    const arr = [];
-    for (let i = 0; i < randomNum(3, 10); i++) {
-        arr.push(randomNum(3, 40));
-    }
-    return arr;
-}
-
-/* 初始化 cubes */
-export function initCubes(values: number[]): ICube[] {
+/** 初始化 cubes */
+export function initCubes(values: number[]): ISortCube[] {
     return values.map((value, index) => ({
-        value,
+        value: value + '',
         strValue: value + '&' + index,
         isActive: false,
         isLock: false,
@@ -22,16 +13,16 @@ export function initCubes(values: number[]): ICube[] {
     }));
 }
 
-/* 根据数组长度，计算出第一个 cube 的 position 的 x 坐标 */
+/** 根据数组长度，计算出第一个 cube 的 position 的 x 坐标 */
 export function getStartXPos(arrLen: number): number {
-    return -(arrLen - 1) * CUBE_INTERVAL_DISTANCE / 2;
+    return -(arrLen - 1) * SORT_CUBE_INTERVAL_DISTANCE / 2;
 }
-/* 传入一组序列，判断其是否有序 */
+/** 传入一组序列，判断其是否有序 */
 export function isSorted(values: number[]): boolean {
     return false;
 }
 
-/* 返回冒泡排序细节 */
+/** 返回冒泡排序细节 */
 export function bubbleSortSeq(arr: number[]) {
     let backup = [...arr];
     let sortSeq = [];
@@ -43,6 +34,7 @@ export function bubbleSortSeq(arr: number[]) {
                 backup[j] = backup[j + 1];
                 backup[j + 1] = temp;
                 sortSeq.push({ type: ActionTypes.Swap, indexes: [j, j + 1] });
+                sortSeq.push({ type: ActionTypes.SwapDone, indexes: [j, j + 1] });
             }
             sortSeq.push({ type: ActionTypes.Deactive, indexes: [j, j + 1] })
         }
@@ -52,7 +44,7 @@ export function bubbleSortSeq(arr: number[]) {
     return sortSeq;
 }
 
-/* 返回选择排序细节 */
+/** 返回选择排序细节 */
 export function selectSortSeq(arr: number[]) {
     let backup = [...arr];
     let sortSeq = [];
@@ -72,9 +64,46 @@ export function selectSortSeq(arr: number[]) {
         backup[i] = max;
         backup[indexOfMax] = temp;
         sortSeq.push({ type: ActionTypes.Swap, indexes: [indexOfMax, i] })
+        sortSeq.push({ type: ActionTypes.SwapDone, indexes: [indexOfMax, i] });
         sortSeq.push({ type: ActionTypes.Lock, indexes: [i] });
     }
     sortSeq.push({ type: ActionTypes.SortDone });
     return sortSeq
 }
+
+/** 返回快速排序细节 */
+export function quickSortSeq(arr: number[], l: number, r: number) {
+    let backup = [...arr]
+    let sortSeq = [];
+    if (l >= r) return;
+
+    let i = l - 1, j = r + 1, x = arr[l + r >> 1];
+    while (i < j) {
+        do {
+            i++;
+            sortSeq.push({ type: ActionTypes.Active, indexes: [i] });
+            sortSeq.push({ type: ActionTypes.Deactive, indexes: [i] });
+        } while (arr[i] < x);
+        sortSeq.push({ type: ActionTypes.Lock, indexes: [i] });
+
+        do {
+            j--;
+            sortSeq.push({ type: ActionTypes.Active, indexes: [j] });
+            sortSeq.push({ type: ActionTypes.Deactive, indexes: [j] });
+        } while (arr[j] > x);
+        sortSeq.push({ type: ActionTypes.Lock, indexes: [i] });
+
+        if (i < j) {
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            sortSeq.push({ type: ActionTypes.Swap, indexes: [i, j] })
+            sortSeq.push({ type: ActionTypes.SwapDone, indexes: [i, j] });
+        }
+    }
+    quickSortSeq(arr, l, j);
+    quickSortSeq(arr, j + 1, r);
+}
+
+
 
