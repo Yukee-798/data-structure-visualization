c218a50b3d243c9b5c684c349312d8ee6b065388
Yukee-798 <909397116@qq.com>
2021-05-15 19:01:15 +0800

    Init linkCube & queueCube.

---
+++ src/pages/LinkedList/LinkCube3d/linkCube3d.tsx
@@ -0,0 +1,92 @@
+import * as THREE from 'three'
+import React, { useEffect, useRef, useState } from 'react'
+import { animated, useSpring, config } from 'react-spring/three'
+import { RoundedBox, Text } from "@react-three/drei";
+import { useFrame } from '@react-three/fiber'
+import { BASE_POSY, SORT_CUBE_INTERVAL_DISTANCE, IGeometryProps } from '../../../types';
+import { quickSortSeq } from '../../../utils/sort';
+
+
+interface ILinkCube3dProps extends IGeometryProps {
+    value: string;
+    startPosX: any;
+}
+
+const LinkCube3d: React.FC<ILinkCube3dProps> = (props) => {
+
+    const {
+        position,
+        isActive,
+        isLock,
+        isReset,
+        value,
+        startPosX,
+        colorConfig,
+    } = props;
+
+    const [isHover, setIsHover] = useState(false)
+    const [isClick, setIsClick] = useState(false)
+    const meshRef = useRef<THREE.Mesh>(null!)
+
+
+    /** 扫描数组的时候，如果改变了 active 属性，则给它设置一个点击效果 */
+    useEffect(() => {
+        isActive ? setIsClick(true) : setIsClick(false);
+    }, [isActive])
+
+    /** 配置扩缩动画效果 */
+    const { scale } = useSpring({
+        reset: isReset,
+        reverse: isReset,
+        from: { scale: 0 },
+        to: { scale: isClick ? 1.10 : 1 },
+        config: isReset ? config.default : config.wobbly
+    })
+
+    /** 配置颜色过渡效果 */
+    const { color } = useSpring({
+        color: (
+            isClick ? colorConfig?.activeColor :
+                isHover ? colorConfig?.hoverColor :
+                    isLock ? colorConfig?.lockColor : colorConfig?.defaultColor
+        )
+    })
+
+    return (
+        <animated.mesh
+            scale={scale}
+            ref={meshRef}
+            position={position}
+        >
+            <Text
+                fontSize={0.5}
+                color='black'
+            >
+                {value}
+            </Text>
+            <RoundedBox
+                args={[2, 1, 1]}
+                onClick={() => setIsClick(!isClick)}
+                onPointerOver={() => setIsHover(true)}
+                onPointerOut={() => setIsHover(false)}
+            >
+                <animated.meshPhongMaterial
+                    color={color}
+                    opacity={0.5}
+                    transparent={true}
+                />
+            </RoundedBox>
+        </animated.mesh>
+    )
+}
+
+LinkCube3d.defaultProps = {
+    colorConfig: {
+        defaultColor: 'wheat',
+        activeColor: 'orange',
+        hoverColor: 'skyblue',
+        lockColor: '#8076a3'
+    }
+}
+
+export default React.memo(LinkCube3d);
