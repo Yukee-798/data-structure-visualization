81d2aca6898aece662a168583b115c7bdd78b131
Yukee-798 <909397116@qq.com>
2021-05-16 18:51:19 +0800

    Update Sphere3d's basic antimation.

--- src/components/Sphere3d/sphere3d.tsx
+++ src/components/Sphere3d/sphere3d.tsx
@@ -1,8 +1,9 @@
 import { IColorConfig, IGeometryProps } from '../../types';
 import { Icosahedron, Text } from '@react-three/drei'
-import { animated } from 'react-spring/three';
+import { animated, config, useSpring } from 'react-spring/three';
+import { useEffect, useRef, useState } from 'react';
 
-interface ISphere3dProps extends IGeometryProps {
+export interface ISphere3dProps extends IGeometryProps {
 
 }
 
@@ -10,30 +11,63 @@ const Sphere3d: React.FC<ISphere3dProps> = (props) => {
 
     const {
         position,
+        value,
         isActive,
         isLock,
         isReset,
         colorConfig,
     } = props;
 
+    const [isHover, setIsHover] = useState(false)
+    const [isClick, setIsClick] = useState(false)
+    const meshRef = useRef<THREE.Mesh>(null!)
+
+    /** 配置颜色过渡效果 */
+    const { color } = useSpring({
+        color: (
+            isClick ? colorConfig?.activeColor :
+                isHover ? colorConfig?.hoverColor :
+                    isLock ? colorConfig?.lockColor : colorConfig?.defaultColor
+        )
+    })
+
+    /** 配置扩缩动画效果 */
+    const { scale } = useSpring({
+        reset: isReset,
+        reverse: isReset,
+        from: { scale: 0 },
+        to: { scale: isClick ? 1.20 : 1 },
+        config: isReset ? config.default : config.wobbly
+    })
+
+    /** 扫描数组的时候，如果改变了 active 属性，则给它设置一个点击效果 */
+    useEffect(() => {
+        isActive ? setIsClick(true) : setIsClick(false);
+    }, [isActive])
+
     return (
         <animated.mesh
             position={position}
+            ref={meshRef}
+            scale={scale}
         >
             <Icosahedron
                 args={[0.7, 10]}
+                onClick={() => setIsClick(!isClick)}
+                onPointerOver={() => setIsHover(true)}
+                onPointerOut={() => setIsHover(false)}
             >
                 <Text
+                    position={[0, 0, 0.7]}
                     fontSize={0.5}
                     color='black'
                 >
-                    123
+                    {value}
                 </Text>
 
                 <animated.meshPhongMaterial
-                    color={colorConfig?.defaultColor}
-                    opacity={0.5}
-                    transparent={true}
+                    color={color}
+                    opacity={1}
                 />
             </Icosahedron>
         </animated.mesh>
@@ -46,8 +80,7 @@ Sphere3d.defaultProps = {
         activeColor: 'orange',
         hoverColor: 'skyblue',
         lockColor: '#8076a3'
-    },
-    isLock: false,
+    }
 }
 
 export default Sphere3d;
