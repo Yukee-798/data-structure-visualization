0bc88ccea5aa91449d38f35e7c56bae340b4fbf9
Yukee-798 <909397116@qq.com>
2021-05-29 16:45:15 +0800

    Add defaultConfig file.

---
+++ src/configs/page/defaultConfig.ts
@@ -0,0 +1,12 @@
+import { IColorConfig } from "../../types";
+
+/**
+ * 该文件用于配置某些组件的默认 props
+ */
+
+export const defaultGeoColor: IColorConfig = {
+    defaultColor: 'wheat',
+    activeColor: 'orange',
+    hoverColor: 'skyblue',
+    lockColor: '#8076a3'
+}
